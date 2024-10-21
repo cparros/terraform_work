228e838ade7562665363094efbca748455e73610
Cparros <celparros@gmail.com>
2024-10-21 13:38:35 -0600

    Refactor main.tf to add security group and user data

--- main.tf
+++ main.tf
@@ -3,10 +3,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World Clayton" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags = {
+    Name = "terraform-example"
+  }
+}
 
-    tags = {
-      Name = "terraform-example-instance"
-    }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    # allow all traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the EC2 instance"
 }
